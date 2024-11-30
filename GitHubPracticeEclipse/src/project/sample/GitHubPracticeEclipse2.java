669b502e1f0e1cd72a58e8bb5aadb602ba0f1615
Chetan Kumar Ganeshi <53119829+chetanganeshi@users.noreply.github.com>
2024-12-01 01:21:00 +0530

    Create GitHubPracticeEclipse2.java

---
+++ GitHubPracticeEclipse/src/project/sample/GitHubPracticeEclipse2.java
@@ -0,0 +1,10 @@
+package project.sample;
+
+public class GitHubPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to GitHub again!");
+	}
+
+}
